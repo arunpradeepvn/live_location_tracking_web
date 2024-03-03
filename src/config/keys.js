e80b0c6100d8062e846381c552b958917c780de0
arunpradeepvn <126814223+arunpradeepvn@users.noreply.github.com>
2024-03-03 10:48:13 +0530

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,5 @@
 const keys = {
-  // google_map_api_key: `AIzaSyDo81ytPTG6fjkGobUmJII_ISKVuW6BxRs`
-  google_map_api_key: `AIzaSyA2fFje29Rgz7upSGefZJrjB-91RrDuNKk`
+  google_map_api_key: `AIzaSyDo81ytPTG6fjkGobUmJII_ISKVuW6BxRs`
 }
 
 export default keys
